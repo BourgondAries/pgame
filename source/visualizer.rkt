c5780286a0fecf3beffd91009919b2be615fbe84
Kevin Robert Stravers <macocio@gmail.com>
2018-08-18 00:32:58 +0200

    visualizer: major cleanup
    Fix indents, add error conditions, and remove old unused code

--- source/visualizer.rkt
+++ source/visualizer.rkt
@@ -5,91 +5,85 @@
 (require racket/file racket/function racket/list racket/port racket/string racket/system
          logger)
 
+;; Settings for tuning the graph
+(define font-size-small 6)
+(define font-size-normal 12)
+(define max-value-length 50)
+(define smaller-font-value-length 30)
 
-(define (normalize graph) (sort (hash->list graph) symbol<? #:key car))
-(define (normalize2 graph) (reverse (sort (hash->list graph) symbol<? #:key car)))
 
-(struct A (parent label value id) #:prefab)
-(define (visualize2 pid label graph id)
+;; Turn a table into a list and sort the result
+(define (normalize graph) (reverse (sort (hash->list graph) symbol<? #:key car)))
+
+;; An A-record
+(struct AnnotatedElement () #:prefab)
+(struct A AnnotatedElement (parent label value id) #:prefab)
+
+;; Recursively converts each hash-table into a list of A
+(define (visualize pid label graph id)
   (cond
     ([hash? graph]
      (for/fold ([build (list (A pid label (void) id))]
                 [value (add1 id)])
-               ([entry (normalize2 graph)])
-       (let-values ([(build* count*) (visualize2 id (car entry) (cdr entry) value)])
+               ([entry (normalize graph)])
+       (let-values ([(build* count*) (visualize id (car entry) (cdr entry) value)])
          (values (cons build* build)
                  count*))))
     (else
+      ;; Incrementing by two so we leave ID space for value nodes in the graph
       (values (A pid label graph id) (+ 2 id)))))
 
+;; Turn any value into a string, ensuring that it's properly escaped
 (define (escape x)
+  (define (string-take str n)
+    (define lstr (string->list str))
+    (if (< (length lstr) n)
+      str
+      (list->string (take (string->list str) n))))
   (string-trim
     (with-output-to-string
-      (thunk (write (string-take (format "~a" x) 18))))
-    "\"")
-  )
+      (thunk (write (string-take (format "~a" x) max-value-length))))
+    "\""))
 
-(define (gvis2 graph)
-  (define-values (g _) (visualize2 0 'root graph 0))
-  (string-append "digraph G { node [shape=oval]; " (annota->dor g) (annotated->dot g) " }"))
+(define (generate-dot-code graph)
+  (define-values (graph* _) (visualize 0 'root graph 0))
+  (string-append "digraph G {\n\tnode [shape=oval];\n" (compute-labels graph*) (annotated-graph->dot graph*) "}"))
 
-(define (annotated->dot agra)
+(define (annotated-graph->dot agra)
+  (define (root-node? agra)
+    (= (A-parent agra) (A-id agra)))
   (cond
-    ([list? agra]  (apply string-append (map annotated->dot agra)))
-    ([A? agra]     (cond
-                     ([= (A-parent agra) (A-id agra)] "") ; Root node
-                     ([void? (A-value agra)] (string-append (escape (A-parent agra)) " -> " (escape (A-id agra)) ";\n"))
-                     (else                   (string-append (escape (A-parent agra)) " -> " (escape (A-id agra)) "-> " (escape (add1 (A-id agra))) ";\n"
-                                                            (escape (add1 (A-id agra))) "[style=filled, fillcolor=orange, shape=box, label=\"" (escape (A-value agra)) "\"];\n"))))
-    (else (crit "Unable to read agraph"))
+    ([list? agra]  (apply string-append (map annotated-graph->dot agra)))
+    ([A?    agra]  (cond
+                     ([root-node? agra]      "")
+                     ([void? (A-value agra)] (string-append "\t" (escape (A-parent agra)) " -> " (escape (A-id agra)) ";\n"))
+                     (else                   (string-append "\t" (escape (A-parent agra)) " -> " (escape (A-id agra)) " -> " (escape (add1 (A-id agra))) ";\n"
+                                                            "\t" (escape (add1 (A-id agra)))
+                                                            (let* ([length (string-length (escape (A-value agra)))]
+                                                                   [size   (if (> length smaller-font-value-length) font-size-small font-size-normal)])
+                                                              (string-append
+                                                                " [style=filled,fillcolor=orange,shape=box,fontsize=" (number->string size) ",label=\""))
+                                                            (escape (A-value agra)) "\"];\n"))))
+    (else (raise-argument-error 'annotated-graph->dot "(or/c list? AnnotatedElement?" agra))))
+
+(define (compute-labels agra)
+  (cond
+    ([list? agra]  (apply string-append (map compute-labels agra)))
+    ([A?    agra]  (string-append "\t" (escape (A-id agra)) " [style=filled,fillcolor=green,label=\"" (escape (A-label agra)) "\"];\n"))
+    (else          (raise-argument-error 'compute-labels "(or/c list? AnnotatedElement?" agra))
   ))
 
-(define (annota->dor agra)
-  (cond
-    ([list? agra]  (apply string-append (map annota->dor agra)))
-    ([A? agra]     (string-append (escape (A-id agra)) "[style=filled, fillcolor=green, label=\"" (escape (A-label agra)) "\"];\n"))
-    (else (crit "Unable to read agraph"))
-  ))
+(define (run-command)
+  (not (system* "/usr/bin/env" "bash" "-c" "dot -T eps temporary/visualizer.dot > temporary/visualizer.eps && evince temporary/visualizer.eps")))
 
-(define (visualize graph #:with-data [data #f])
-  (define (visualize* data graph parent)
-    (cond
-      ([hash? graph]
-       (for/list ([entry (normalize graph)])
-         (cons
-          (string-append "\"" (symbol->string parent) "\" -> \"" (symbol->string (car entry)) "\";")
-          (visualize* data (cdr entry) (car entry))
-         )))
-      (else (if data (string-append "\"" (symbol->string parent) "\" -> \""
-                           (string-trim
-                             (with-output-to-string
-                               (thunk (write (string-take (format "~v" graph) 18))))
-                             "\"")
-                           "\";")
-              ""))))
-  (format "digraph G {\n~a\n}"
-    (string-join
-      (flatten
-        (for/list ([entry (normalize graph)])
-          (visualize* data (cdr entry) (car entry))))
-      "\n")))
-
-(define (string-take str n)
-  (define lstr (string->list str))
-  (if (< (length lstr) n)
-    str
-    (list->string (take (string->list str) n))))
-
-(define (show-visualization graph #:with-data [data #t])
-  ; (define (call-with-values (thunk (visualize2 0 'root graph 0)) (lambda (x y) (annotated->dot x)))
-  (define temporary-file2 "temporary/visualizer2.dot")
-  (with-output-to-file temporary-file2 #:exists 'replace
-    (thunk (display (gvis2 graph))))
-  (system* "/usr/bin/env" "bash" "-c" "dot -T eps temporary/visualizer2.dot > temporary/visualizer2.eps && evince temporary/visualizer2.eps")
-
+(define (show-visualization graph)
   (define temporary-file "temporary/visualizer.dot")
   (with-output-to-file temporary-file #:exists 'replace
-    (thunk (display (visualize graph #:with-data data))))
-  (system* "/usr/bin/env" "bash" "-c" "dot -T eps temporary/visualizer.dot > temporary/visualizer.eps && evince temporary/visualizer.eps")
-  )
+    (thunk (display (generate-dot-code graph))))
+  (define command-error
+    (call-with-output-string
+      (lambda (p) (parameterize ([current-error-port p])
+                    (run-command)))))
+  (when (non-empty-string? command-error)
+    (crit+ command-error)))
 
