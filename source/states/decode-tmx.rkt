3ec59590fb79827ca4f1f87b98186685303ae770
Kevin Robert Stravers <macocio@gmail.com>
2018-10-26 23:29:56 +0200

    Remove the state macro!
    Hurrah! This unscalable monster is gone!

--- source/states/decode-tmx.rkt
+++ source/states/decode-tmx.rkt
@@ -5,7 +5,7 @@
                      threading)
          ffi/vector finalizer math/matrix opengl opengl/util threading
          glfw3 logger memo nested-hash spipe xml
-         "../drawing.rkt" "../impure.rkt" "../pure.rkt" "../shutdown.rkt" "../state.rkt")
+         "../drawing.rkt" "../impure.rkt" "../pure.rkt" "../shutdown.rkt")
 
 (provide (all-defined-out))
 
@@ -18,16 +18,8 @@
 (define (t)
   (x "data/town-2.tmx"))
 
-(state decode-tmx
-  (enter
-    (t ())
-   )
-  (pre
-    (clear-keys (io.window) ae.keys)
-   )
-  (pure
-    (pop-fsm fsm)
-    )
-  (post)
-  (exit))
+(define/H~> decode-tmx
+  (t ())
+  (clear-keys (io.window) ae.keys)
+  (pop-fsm fsm))
 
